742a66effa37d4dbc20da9a74c7c09e7748088f6
Baldwin, Josiah <jbaldwin@nwvariancesoftware.dev>
2022-05-27 21:48:22 -0700

    Moved categories to own module

--- src/lib.rs
+++ src/lib.rs
@@ -3,25 +3,35 @@
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum Tile {
-    Suit(Suit),
-    Honor(Honor),
-    Bonus(Bonus),
+    Suit(category::Suit),
+    Honor(category::Honor),
+    Bonus(category::Bonus),
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub enum Suit {
-    /// Circles
-    Pin(i32),
-    /// Bamboo
-    So(i32),
-    /// Characters
-    Man(i32),
-}
+pub mod category {
+    use super::*;
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub enum Honor {
-    Winds(Winds),
-    Dragons(Dragons),
+    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
+    pub enum Suit {
+        /// Circles
+        Pin(i32),
+        /// Bamboo
+        So(i32),
+        /// Characters
+        Man(i32),
+    }
+
+    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
+    pub enum Bonus {
+        Seasons(Seasons),
+        Flowers(Flowers),
+    }
+
+    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
+    pub enum Honor {
+        Winds(Winds),
+        Dragons(Dragons),
+    }
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
@@ -37,12 +47,6 @@ pub enum Dragons {
     Red,
     Green,
     White,
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub enum Bonus {
-    Seasons(Seasons),
-    Flowers(Flowers),
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
@@ -63,5 +67,5 @@ pub enum Flowers {
 
 // Tiles list
 // Pin
-pub const IIPIN: Suit = Suit::Pin(1);
-pub const RYANPIN: Suit = Suit::Pin(2);
+pub const IIPIN: category::Suit = category::Suit::Pin(1);
+pub const RYANPIN: category::Suit = category::Suit::Pin(2);
