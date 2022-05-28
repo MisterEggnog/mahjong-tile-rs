32a46c9352c4941cc79ba799a5a145b2b922a50e
Baldwin, Josiah <jbaldwin@nwvariancesoftware.dev>
2022-05-27 21:21:56 -0700

    Use enum not struct + enum

--- src/lib.rs
+++ src/lib.rs
@@ -2,38 +2,16 @@
 #![allow(unused_imports)]
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub struct SuitTile {
-    suit: Suit,
-    number: i32,
-}
-
-impl SuitTile {
-    pub fn get_suit(&self) -> Suit {
-        self.suit
-    }
-
-    pub fn get_number(&self) -> i32 {
-        self.number
-    }
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum Suit {
     /// Circles
-    Pin,
+    Pin(i32),
     /// Bamboo
-    So,
+    So(i32),
     /// Characters
-    Man,
+    Man(i32),
 }
 
 // Tiles list
 // Pin
-pub const IIPIN: SuitTile = SuitTile {
-    suit: Suit::Pin,
-    number: 1,
-};
-pub const RYANPIN: SuitTile = SuitTile {
-    suit: Suit::Pin,
-    number: 2,
-};
+pub const IIPIN: Suit = Suit::Pin(1);
+pub const RYANPIN: Suit = Suit::Pin(2);
