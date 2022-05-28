6c9e7b66f48d922cd2005e5f167ebbfeb68a9b3f
Baldwin, Josiah <jbaldwin@nwvariancesoftware.dev>
2022-05-28 12:36:56 -0700

    Reverted tile submodules
    Revert "Moved honors before bonus"
    
    This reverts commit 1094e550634817aebdca1e976897f2c222c33ae1.
    
    Revert "Moved ranks to own module"
    
    This reverts commit e59aadc31998ac4e6d9beecebda86a2970e34159.
    
    Revert "Moved categories to own module"
    
    This reverts commit 742a66effa37d4dbc20da9a74c7c09e7748088f6.

--- src/lib.rs
+++ src/lib.rs
@@ -3,72 +3,65 @@
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum Tile {
-    Suit(category::Suit),
-    Honor(category::Honor),
-    Bonus(category::Bonus),
+    Suit(Suit),
+    Honor(Honor),
+    Bonus(Bonus),
 }
 
-pub mod category {
-    use super::*;
-
-    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-    pub enum Suit {
-        /// Circles
-        Pin(i32),
-        /// Bamboo
-        So(i32),
-        /// Characters
-        Man(i32),
-    }
-
-    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-    pub enum Honor {
-        Winds(ranks::Winds),
-        Dragons(ranks::Dragons),
-    }
-
-    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-    pub enum Bonus {
-        Seasons(ranks::Seasons),
-        Flowers(ranks::Flowers),
-    }
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Suit {
+    /// Circles
+    Pin(i32),
+    /// Bamboo
+    So(i32),
+    /// Characters
+    Man(i32),
 }
 
-mod ranks {
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Honor {
+    Winds(Winds),
+    Dragons(Dragons),
+}
 
-    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-    pub enum Winds {
-        East,
-        South,
-        West,
-        North,
-    }
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Winds {
+    East,
+    South,
+    West,
+    North,
+}
 
-    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-    pub enum Dragons {
-        Red,
-        Green,
-        White,
-    }
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Dragons {
+    Red,
+    Green,
+    White,
+}
 
-    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-    pub enum Seasons {
-        Spring,
-        Summer,
-        Autumn,
-        Winter,
-    }
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Bonus {
+    Seasons(Seasons),
+    Flowers(Flowers),
+}
 
-    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
-    pub enum Flowers {
-        Plum,
-        Orchid,
-        Chrysanthemum,
-        Bamboo,
-    }
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Seasons {
+    Spring,
+    Summer,
+    Autumn,
+    Winter,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Flowers {
+    Plum,
+    Orchid,
+    Chrysanthemum,
+    Bamboo,
 }
 
 // Tiles list
 // Pin
-pub const IIPIN: category::Suit = category::Suit::Pin(1);
-pub const RYANPIN: category::Suit = category::Suit::Pin(2);
+pub const IIPIN: Suit = Suit::Pin(1);
+pub const RYANPIN: Suit = Suit::Pin(2);
