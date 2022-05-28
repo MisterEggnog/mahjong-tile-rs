8e2c9fc9e6d4afc4a858ee3d13bb70ccd9d132a0
Baldwin, Josiah <jbaldwin@nwvariancesoftware.dev>
2022-05-27 21:00:37 -0700

    Ran cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,40 @@
-
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub struct MahjongTile {
-	suit: Suit,
-	number: i32,
+    suit: Suit,
+    number: i32,
 }
 
 impl MahjongTile {
-	pub fn get_suit(&self) -> Suit {
-		self.suit
-	}
+    pub fn get_suit(&self) -> Suit {
+        self.suit
+    }
 
-	pub fn get_number(&self) -> i32 {
-		self.number
-	}
+    pub fn get_number(&self) -> i32 {
+        self.number
+    }
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum Suit {
-	/// Circles
-	Pin,
-	/// Bamboo
-	So,
-	/// Characters
-	Man,
-	/// Not a true suit, only 4 tiles
-	Wind,
-	/// Not a true suit, on 3 tiles
-	Dragon,
+    /// Circles
+    Pin,
+    /// Bamboo
+    So,
+    /// Characters
+    Man,
+    /// Not a true suit, only 4 tiles
+    Wind,
+    /// Not a true suit, on 3 tiles
+    Dragon,
 }
 
 // Tiles list
 // Pin
-pub const iipin: MahjongTile = MahjongTile { suit: Suit::Pin, number: 1 };
-pub const ryanpin: MahjongTile = MahjongTile { suit: Suit::Pin, number: 2 };
+pub const iipin: MahjongTile = MahjongTile {
+    suit: Suit::Pin,
+    number: 1,
+};
+pub const ryanpin: MahjongTile = MahjongTile {
+    suit: Suit::Pin,
+    number: 2,
+};
