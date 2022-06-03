6977253b2433f3a3e87c3cbe2cb951e6adc43387
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-06-02 18:13:56 -0700

    Renamed internal bounded type

--- src/lib.rs
+++ src/lib.rs
@@ -10,16 +10,16 @@ pub enum Tile {
     Bonus(Bonus),
 }
 
-type SuitNum = bounded_integer::BoundedU8<1, 9>;
+type BdU8<const A: u8, const B: u8> = bounded_integer::BoundedU8<A, B>;
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum Suit {
     /// Circles
-    Circles(SuitNum),
+    Circles(BdU8<1, 9>),
     /// Bamboo
-    Bamboo(SuitNum),
+    Bamboo(BdU8<1, 9>),
     /// Characters
-    Characters(SuitNum),
+    Characters(BdU8<1, 9>),
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
@@ -67,11 +67,11 @@ pub enum Flowers {
 
 // Tiles list
 // Pin
-pub const IIPIN: Suit = Suit::Circles(match SuitNum::new(1) {
+pub const IIPIN: Suit = Suit::Circles(match BdU8::new(1) {
     Some(a) => a,
     None => unreachable!(),
 });
-pub const RYANPIN: Suit = Suit::Circles(match SuitNum::new(2) {
+pub const RYANPIN: Suit = Suit::Circles(match BdU8::new(2) {
     Some(a) => a,
     None => unreachable!(),
 });
