df80444067697c4e2908a75a6b2e6df0e324cf41
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-06-02 21:14:06 -0700

    Iterator for winds

--- src/lib.rs
+++ src/lib.rs
@@ -87,6 +87,14 @@ impl Dragons {
     }
 }
 
+impl Winds {
+    pub fn members() -> impl Iterator<Item = Tile> {
+        [Winds::East, Winds::South, Winds::West, Winds::North]
+            .into_iter()
+            .map(|w| Tile::Honor(Honor::Winds(w)))
+    }
+}
+
 #[test]
 fn verify_suit_amount() {
     let suits = Suit::members();
@@ -102,6 +110,12 @@ fn verify_suit_amount() {
 fn verify_dragon_amount() {
     let dragons = Dragons::members();
     assert_eq!(3, dragons.count());
+}
+
+#[test]
+fn verify_winds_amount() {
+    let winds = Winds::members();
+    assert_eq!(4, winds.count());
 }
 
 // Tiles list
