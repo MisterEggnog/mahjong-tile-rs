3e7edf23d1cfbd535dc13905c1b7a4d7f6bf96bc
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-06-03 17:07:58 -0700

    utility function for loop iterator using closure

--- src/utility.rs
+++ src/utility.rs
@@ -4,8 +4,19 @@ pub fn loop_iterator<I: Iterator + Clone>(iter: I, loop_num: usize) -> impl Iter
     std::iter::repeat(iter).take(loop_num).flatten()
 }
 
+pub fn loop_iterator_with<I, F>(iter: F, loop_num: usize) -> impl Iterator
+where
+    I: Iterator,
+    F: FnMut() -> I,
+{
+    std::iter::repeat_with(iter).take(loop_num).flatten()
+}
+
 #[test]
 fn loop_three_times() {
     let iter = loop_iterator(1..=10, 3);
+    assert_eq!(30, iter.count());
+
+    let iter = loop_iterator_with(|| 1..=10, 3);
     assert_eq!(30, iter.count());
 }
