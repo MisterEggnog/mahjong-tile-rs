81fe244cc9bd18bec1385d66cde210733da5a622
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-06-08 19:02:48 -0700

    char test handles str now

--- tests/to_char.rs
+++ tests/to_char.rs
@@ -4,7 +4,19 @@ use std::iter::once;
 #[test]
 fn numerical_unicode_equality() {
     let mahjong_chars = (0x1F000..=0x1F02B_u32)
-        .map(|c| char::from_u32(c).expect("Supposed to be part of unicode SMP"));
+        .map(|c| {
+            format!(
+                "{}",
+                char::from_u32(c).expect("Supposed to be part of unicode SMP")
+            )
+        })
+        .map(|c| {
+            if c == "🀄" {
+                "🀄︎".to_string()
+            } else {
+                c
+            }
+        });
     let tiles = Suit::members()
         .chain(Dragons::members())
         .chain(Winds::members())
@@ -12,8 +24,13 @@ fn numerical_unicode_equality() {
         .chain(Flowers::members())
         .chain(once(Tile::Special(Special::Joker)))
         .chain(once(Tile::Special(Special::Black)));
-    let mut tiles: Vec<(char, Tile)> = tiles
-        .map(|c| (char::try_from(c).expect("These should not fail"), c))
+    let mut tiles: Vec<(&'static str, Tile)> = tiles
+        .map(|c| {
+            (
+                <&'static str>::try_from(c).expect("These should not fail"),
+                c,
+            )
+        })
         .collect();
     tiles.sort_unstable_by(|a, b| a.0.cmp(&b.0));
 
