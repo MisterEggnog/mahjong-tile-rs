4c6b453b8aafb38c94618482c047c6f6915f097f
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-06-15 15:06:53 -0700

    Partial test for dragons to_char

--- src/honor.rs
+++ src/honor.rs
@@ -59,3 +59,16 @@ fn verify_winds_amount() {
     let winds = Winds::members();
     assert_eq!(4, winds.count());
 }
+
+#[test]
+fn verify_dragons_to_char() {
+    use crate::Tile;
+    use std::collections::HashSet;
+    let dragons_char_uniq = Dragons::members()
+        .map(|t| match t {
+            Tile::Honor(Honor::Dragons(d)) => d,
+            _ => panic!("Impossible value {:?}", t),
+        })
+        .map(From::from::<char>)
+        .collect::<HashSet<char>>();
+}
